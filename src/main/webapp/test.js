943b261d1b0e3742102313e83a0b38f6c6243b63
st <jvprofessor649@gmail.com>
2021-10-26 09:12:21 -0500

    css and js

--- src/main/webapp/test.js
+++ src/main/webapp/test.js
@@ -1,63 +1,19 @@
-checkDevice();
-function checkDevice(){
-	var a=window.navigator.userAgent;
-//	alert(a);
-}
 
-$(document).ready(function() {
-	$("#loginsubmit").click(validatelogin);
-	$("#reguid").blur(checkUser);
-	$(".prodDisplay").click(animate);
-	loginAttemptCheck();
-	/*
-		// move mouse over, i want to call "header()" js function
-		$("#header").mouseover(  header  );
-		access to element = $("#header")
-		event listenere = .mouseover
-		what function to call = header	
-	*/
-});
-function checkUser(){
-
-	var checkUserAjaxObj={
-	        url: '/doesUserExist',
-	        type: 'post',
-	        data: {
-	        	registerUser:$("#reguid").val()
-			},
-	        context: this,
-		    success: function (data) {
-	        		if(data=="true")
-	        			alert("userid already taken");
-	        		else
-	        			alert("userid available");
-		        },
-	        error: function (data) {
-	        	console.log("failure");
-	        }
-	};
-	
-	$.ajax(  checkUserAjaxObj  );
-	
-}
-function animate(){
-	$(".prodDisplay").slideUp(10000);
-}
 function validatelogin(){
 	// collect whatever customer typed in the login section
 	
-	//var customerEnteredUid=document.getElementById("uid").value;
-	var customerEnteredUid=$("#uid").val();
+	var customerEnteredUid=document.getElementById("uid").value;
+	//var customerEnteredUid=$("#uid").val();
 	
-	//var customerEnteredpwd=document.getElementById("pwd").value;
-	var customerEnteredpwd=$("#pwd").val();
+	var customerEnteredpwd=document.getElementById("pwd").value;
+	//var customerEnteredpwd=$("#pwd").val();
 	
 	// verify all login info is entered correctly
 	if(customerEnteredUid == ""  )
 	{
-		$("#uid").val("please enter");
-		//document.getElementById("uid").style.background="red";
-		$("#uid").css("background","purple");
+		//$("#uid").val("please enter");
+		document.getElementById("uid").style.background="red";
+		//$("#uid").css("background","purple");
 		alert(" userid is mandatory");
 		return false;
 	}
@@ -69,26 +25,6 @@ function validatelogin(){
 	}
 	else
 		return true;
-	
-
-/*
-	var customerEnteredUid=$("#uid").val();
-	var customerEnteredpwd=$(".pass").val();
-	if(customerEnteredUid == "" || customerEnteredpwd == "")
-		alert(" userid and password is mandatory");
-	if(customerEnteredUid.indexOf("@")  == -1 ){
-		alert("invalid userid id");
-		$("#uid").css("background","red");
-		$("#submit").hide();
-	}
-	$("#uid") - access html element by "id" attribute
-	$(".pass") - access html element by "class" attribute
-	$("label") - access html element by element name
-	$("#products").fadeOut(3000);
-	$("#uid").css("background","blue");
-	$("#uid").val("set value");
-	
-*/	
 	
 }
 
@@ -120,17 +56,21 @@ function filterClicked(){
 	console.log("customer just clicked filter section of webpage");
 }
 var xyz=123;
+
 function   test( a, b  ){
 	
 	// basic primitive data type
 	var v="john";
+	v=true;
+	
 	var b=true;
 	var c='x';
 	var d= 10;
 	var e=10.0;
-	 
+		
 	var f=[1,4,5,6];
-	console.log(f);
+	console.log(f [3] );
+	// System.out.print
 	console.log("welcome to javascript");
 	// object data type
 	var student={
@@ -150,7 +90,7 @@ function   test( a, b  ){
 	
 }
 
-
+add(1,2);
 function add( a, b){
 
 	for(i=0; i < 3;i++)
@@ -172,7 +112,7 @@ function add( a, b){
 		console.log("in do while loop");
 		break;
 	}while(true);
-	
+	 
 	try{
 		var aa=abc.split("a");
 	}catch(e){
@@ -181,7 +121,7 @@ function add( a, b){
 		console.log("finally block");
 	}
 	
-	var email="abcg.com";
+	var email="abcd.com";
 	console.log(email);
 	email=email.replace("a","c");
 	console.log(email);
@@ -189,7 +129,7 @@ function add( a, b){
 	console.log(arr);
 	
 	if(email.indexOf("@")  == -1 )
-		alert("invalid email id");
+		alert("invalid email id"); // popup window
 	else
 		alert("valid email id");
 
@@ -210,6 +150,75 @@ function abc(){
 
 
 
+
+
+
+/*
+checkDevice();
+function checkDevice(){
+	var a=window.navigator.userAgent;
+//	alert(a);
+}
+
+$(document).ready(function() {
+	$("#loginsubmit").click(validatelogin);
+	$("#reguid").blur(checkUser);
+	$(".prodDisplay").click(animate);
+	loginAttemptCheck();
+	
+});
+
+function checkUser(){
+
+	var checkUserAjaxObj={
+	        url: '/doesUserExist',
+	        type: 'post',
+	        data: {
+	        	registerUser:$("#reguid").val()
+			},
+	        context: this,
+		    success: function (data) {
+	        		if(data=="true")
+	        			alert("userid already taken");
+	        		else
+	        			alert("userid available");
+		        },
+	        error: function (data) {
+	        	console.log("failure");
+	        }
+	};
+	
+	$.ajax(  checkUserAjaxObj  );
+	
+}
+function animate(){
+	$(".prodDisplay").slideUp(10000);
+}
+*/
+
+
+
+/*
+	var customerEnteredUid=$("#uid").val();
+	var customerEnteredpwd=$(".pass").val();
+	if(customerEnteredUid == "" || customerEnteredpwd == "")
+		alert(" userid and password is mandatory");
+	if(customerEnteredUid.indexOf("@")  == -1 ){
+		alert("invalid userid id");
+		$("#uid").css("background","red");
+		$("#submit").hide();
+	}
+	$("#uid") - access html element by "id" attribute
+	$(".pass") - access html element by "class" attribute
+	$("label") - access html element by element name
+	$("#products").fadeOut(3000);
+	$("#uid").css("background","blue");
+	$("#uid").val("set value");
+	
+*/	
+	
+	
+/*
 function loginAttemptCheck(){
 
 	var loginAttemptCheck={
@@ -228,3 +237,4 @@ function loginAttemptCheck(){
 	
 }
 
+*/
